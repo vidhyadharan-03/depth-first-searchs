1902df7f52b5a431606bfb371a653a72e3083e61
Vidhyadharan <114286357+vidhyadharan-03@users.noreply.github.com>
2024-08-24 15:55:36 +0530

    Update dfs.py
    code sightly modified

--- dfs.py
+++ dfs.py
@@ -1,24 +1,20 @@
-
-
-'''Depth First Search uses STACK AND RECURSION
-'''
-#import defaultdict
 from collections import defaultdict
-def dfs(graph,start,visited,path):
+def dfs(graph, start, visited, path):
+    visited[start] = True
     path.append(start)
-    visited[start]=True
     for neighbour in graph[start]:
-         if not visited[neighbour]:
-            path = dfs(graph, neighbour, visited, path)
-    return path
-graph=defaultdict(list)
-n,e=map(int,input().split())
-for i in range(e):
-    u, v = map(str, input().split())
+        if not visited[neighbour]:
+            dfs(graph, neighbour, visited, path)
+graph = defaultdict(list)
+n, e = map(int, input().split())
+for _ in range(e):
+    u, v = input().split()
     graph[u].append(v)
-#print(graph)
-start='A'
-visited=defaultdict(bool)
- #type ur code here
-traversedpath=dfs(graph,start,visited,path)
-print(traversedpath)
+if '0' in graph:
+    start = '0'
+else:
+    start = 'A'
+visited = defaultdict(bool)
+path = []
+dfs(graph, start, visited, path)
+print(path)
